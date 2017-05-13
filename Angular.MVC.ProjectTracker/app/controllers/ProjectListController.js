8368960d388c2d4d88c580bea75690b1f546d047
Alex <jhnsoap@gmail.com>
2017-05-13 11:13:59 +1000

    Latest Work

--- Angular.MVC.ProjectTracker/app/controllers/ProjectListController.js
+++ Angular.MVC.ProjectTracker/app/controllers/ProjectListController.js
@@ -1,6 +1,7 @@
 ﻿ProjectTracker.controller('ProjectListController',
-    function ProjectListController($scope) {
+    function ProjectListController($scope,$locale) {
         $scope.CheckItsWorking = "It Worked!";
+        $scope.ShortDateFormat = $locale.DATETIME_FORMATS.shortDate;
         $scope.projects = [{ startdate: new Date(), title: 'Static Sample', status: 'I', priority: 'H' },
                            { startdate: new Date(), title: 'Static Sample 2', status: 'C', priority: 'M' }];
     });
